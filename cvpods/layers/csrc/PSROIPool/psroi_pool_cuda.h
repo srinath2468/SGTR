117748a9122c6d8f74d676237a9cda1e3cdd93b3
srinath.rao.klens <srinath.rao@k-lens.de>
2025-03-16 19:15:08 +0100

    remove THC

--- cvpods/layers/csrc/PSROIPool/psroi_pool_cuda.h
+++ cvpods/layers/csrc/PSROIPool/psroi_pool_cuda.h
@@ -1,28 +1,16 @@
-#include <torch/types.h>
-#include <torch/extension.h>
 #include <ATen/ATen.h>
-#include <THC/THC.h>
 #include <math.h>
 #include <stdio.h>
+#include <torch/extension.h>
+#include <torch/types.h>
 
-namespace cvpods {
-at::Tensor psroi_pooling_forward_cuda(
-    at::Tensor& features,
-    at::Tensor& rois,
-    at::Tensor& mappingchannel,
-    const int pooled_height,
-    const int pooled_width,
-    const float spatial_scale,
-    const int group_size,
-    const int output_dim);
+namespace cvpods
+{
+at::Tensor psroi_pooling_forward_cuda(at::Tensor& features, at::Tensor& rois, at::Tensor& mappingchannel,
+                                      const int pooled_height, const int pooled_width, const float spatial_scale,
+                                      const int group_size, const int output_dim);
 
-at::Tensor psroi_pooling_backward_cuda(
-    at::Tensor& top_grad,
-    at::Tensor& rois,
-    at::Tensor& mappingchannel,
-    const int batch_size,
-    const int bottom_dim,
-    const int bottom_height,
-    const int bottom_width,
-    const float spatial_scale);
-}
+at::Tensor psroi_pooling_backward_cuda(at::Tensor& top_grad, at::Tensor& rois, at::Tensor& mappingchannel,
+                                       const int batch_size, const int bottom_dim, const int bottom_height,
+                                       const int bottom_width, const float spatial_scale);
+}  // namespace cvpods
--- cvpods/layers/csrc/ml_nms/ml_nms.cu
+++ cvpods/layers/csrc/ml_nms/ml_nms.cu
@@ -2,15 +2,22 @@
 #include <ATen/ATen.h>
 #include <ATen/cuda/CUDAContext.h>
 
-#include <THC/THC.h>
-#include <THC/THCDeviceUtils.cuh>
-
-#include <vector>
 #include <iostream>
+#include <vector>
+
+#define CUDA_CHECK(err) \
+  do { \
+    if (err != cudaSuccess) { \
+      printf("[CUDA Error] %s (code: %d) at %s:%d\n", cudaGetErrorString(err), err, __FILE__, __LINE__); \
+    } \
+  } while (0)
 
 int const threadsPerBlock = sizeof(unsigned long long) * 8;
 
-__device__ inline float devIoU(float const * const a, float const * const b) {
+#define THCCeilDiv(a, b) (((a) + (b) -1) / (b))
+
+__device__ inline float devIoU(float const* const a, float const* const b)
+{
   if (a[5] != b[5]) {
     return 0.0;
   }
@@ -23,38 +30,31 @@ __device__ inline float devIoU(float const * const a, float const * const b) {
   return interS / (Sa + Sb - interS);
 }
 
-__global__ void ml_nms_kernel(const int n_boxes, const float nms_overlap_thresh,
-                           const float *dev_boxes, unsigned long long *dev_mask) {
+__global__ void ml_nms_kernel(const int n_boxes, const float nms_overlap_thresh, const float* dev_boxes,
+                              unsigned long long* dev_mask)
+{
   const int row_start = blockIdx.y;
   const int col_start = blockIdx.x;
 
   // if (row_start > col_start) return;
 
-  const int row_size =
-        min(n_boxes - row_start * threadsPerBlock, threadsPerBlock);
-  const int col_size =
-        min(n_boxes - col_start * threadsPerBlock, threadsPerBlock);
+  const int row_size = min(n_boxes - row_start * threadsPerBlock, threadsPerBlock);
+  const int col_size = min(n_boxes - col_start * threadsPerBlock, threadsPerBlock);
 
   __shared__ float block_boxes[threadsPerBlock * 6];
   if (threadIdx.x < col_size) {
-    block_boxes[threadIdx.x * 6 + 0] =
-        dev_boxes[(threadsPerBlock * col_start + threadIdx.x) * 6 + 0];
-    block_boxes[threadIdx.x * 6 + 1] =
-        dev_boxes[(threadsPerBlock * col_start + threadIdx.x) * 6 + 1];
-    block_boxes[threadIdx.x * 6 + 2] =
-        dev_boxes[(threadsPerBlock * col_start + threadIdx.x) * 6 + 2];
-    block_boxes[threadIdx.x * 6 + 3] =
-        dev_boxes[(threadsPerBlock * col_start + threadIdx.x) * 6 + 3];
-    block_boxes[threadIdx.x * 6 + 4] =
-        dev_boxes[(threadsPerBlock * col_start + threadIdx.x) * 6 + 4];
-    block_boxes[threadIdx.x * 6 + 5] =
-        dev_boxes[(threadsPerBlock * col_start + threadIdx.x) * 6 + 5];
+    block_boxes[threadIdx.x * 6 + 0] = dev_boxes[(threadsPerBlock * col_start + threadIdx.x) * 6 + 0];
+    block_boxes[threadIdx.x * 6 + 1] = dev_boxes[(threadsPerBlock * col_start + threadIdx.x) * 6 + 1];
+    block_boxes[threadIdx.x * 6 + 2] = dev_boxes[(threadsPerBlock * col_start + threadIdx.x) * 6 + 2];
+    block_boxes[threadIdx.x * 6 + 3] = dev_boxes[(threadsPerBlock * col_start + threadIdx.x) * 6 + 3];
+    block_boxes[threadIdx.x * 6 + 4] = dev_boxes[(threadsPerBlock * col_start + threadIdx.x) * 6 + 4];
+    block_boxes[threadIdx.x * 6 + 5] = dev_boxes[(threadsPerBlock * col_start + threadIdx.x) * 6 + 5];
   }
   __syncthreads();
 
   if (threadIdx.x < row_size) {
     const int cur_box_idx = threadsPerBlock * row_start + threadIdx.x;
-    const float *cur_box = dev_boxes + cur_box_idx * 6;
+    const float* cur_box = dev_boxes + cur_box_idx * 6;
     int i = 0;
     unsigned long long t = 0;
     int start = 0;
@@ -71,9 +71,11 @@ __global__ void ml_nms_kernel(const int n_boxes, const float nms_overlap_thresh,
   }
 }
 
-namespace cvpods {
+namespace cvpods
+{
 // boxes is a N x 6 tensor
-at::Tensor ml_nms_cuda(const at::Tensor boxes, float nms_overlap_thresh) {
+at::Tensor ml_nms_cuda(const at::Tensor boxes, float nms_overlap_thresh)
+{
   using scalar_t = float;
   AT_ASSERTM(boxes.type().is_cuda(), "boxes must be a CUDA tensor");
   auto scores = boxes.select(1, 4);
@@ -86,27 +88,19 @@ at::Tensor ml_nms_cuda(const at::Tensor boxes, float nms_overlap_thresh) {
 
   scalar_t* boxes_dev = boxes_sorted.data_ptr<scalar_t>();
 
-  THCState *state = at::globalContext().lazyInitCUDA(); // TODO replace with getTHCState
+  assert(at::cuda::is_available());
 
   unsigned long long* mask_dev = NULL;
-  //THCudaCheck(THCudaMalloc(state, (void**) &mask_dev,
-  //                      boxes_num * col_blocks * sizeof(unsigned long long)));
 
-  mask_dev = (unsigned long long*) THCudaMalloc(state, boxes_num * col_blocks * sizeof(unsigned long long));
+  cudaMalloc((void**&) mask_dev, boxes_num * col_blocks * sizeof(unsigned long long));
 
-  dim3 blocks(THCCeilDiv(boxes_num, threadsPerBlock),
-              THCCeilDiv(boxes_num, threadsPerBlock));
+  dim3 blocks(THCCeilDiv(boxes_num, threadsPerBlock), THCCeilDiv(boxes_num, threadsPerBlock));
   dim3 threads(threadsPerBlock);
-  ml_nms_kernel<<<blocks, threads>>>(boxes_num,
-                                  nms_overlap_thresh,
-                                  boxes_dev,
-                                  mask_dev);
+  ml_nms_kernel<<<blocks, threads>>>(boxes_num, nms_overlap_thresh, boxes_dev, mask_dev);
 
   std::vector<unsigned long long> mask_host(boxes_num * col_blocks);
-  THCudaCheck(cudaMemcpy(&mask_host[0],
-                        mask_dev,
-                        sizeof(unsigned long long) * boxes_num * col_blocks,
-                        cudaMemcpyDeviceToHost));
+  CUDA_CHECK(
+      cudaMemcpy(&mask_host[0], mask_dev, sizeof(unsigned long long) * boxes_num * col_blocks, cudaMemcpyDeviceToHost));
 
   std::vector<unsigned long long> remv(col_blocks);
   memset(&remv[0], 0, sizeof(unsigned long long) * col_blocks);
@@ -121,19 +115,19 @@ at::Tensor ml_nms_cuda(const at::Tensor boxes, float nms_overlap_thresh) {
 
     if (!(remv[nblock] & (1ULL << inblock))) {
       keep_out[num_to_keep++] = i;
-      unsigned long long *p = &mask_host[0] + i * col_blocks;
+      unsigned long long* p = &mask_host[0] + i * col_blocks;
       for (int j = nblock; j < col_blocks; j++) {
         remv[j] |= p[j];
       }
     }
   }
 
-  THCudaFree(state, mask_dev);
+  cudaFree(mask_dev);
   // TODO improve this part
-  return std::get<0>(order_t.index({
-                       keep.narrow(/*dim=*/0, /*start=*/0, /*length=*/num_to_keep).to(
-                         order_t.device(), keep.scalar_type())
-                     }).sort(0, false));
+  return std::get<0>(
+      order_t
+          .index({keep.narrow(/*dim=*/0, /*start=*/0, /*length=*/num_to_keep).to(order_t.device(), keep.scalar_type())})
+          .sort(0, false));
 }
 
-}
+}  // namespace cvpods
